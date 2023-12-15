3cdd31e5dbb2962b78db25a934d19e6df33e682e
Stephan Höhn <me@steph.ovh>
2023-12-15 12:05:43 +0100

    test: adjusts kubernetes watcher test so it works again
    had to adjust the test for kubernetes_watcher.py again so it will work with the new logic

--- tests/kubernetes_utils/test_kubernetes_watcher.py
+++ tests/kubernetes_utils/test_kubernetes_watcher.py
@@ -6,131 +6,173 @@ from src.storage.ingress_entity import IngressEntity
 from src.storage.storage_service import StorageService
 from src.zeroconf_utils.zeroconf_service import ZeroconfService
 
-class TestkubernetesWatcher(unittest.TestCase):
-    def test_StartAddedEvent(self):
+
+class TestKubernetesWatcher(unittest.TestCase):
+    def test_added_ingress(self):
         rule = Mock()
-        
+
         rule.host = "test.local"
-        
+
         ingress = Mock()
-        
+
+        ingress.metadata.namespace = "TestNamespace"
+
+        ingress.metadata.name = "TestIngress"
+
         ingress.spec.rules = [
             rule
         ]
-        
+
+        ip = Mock()
+
+        ip.ip = "192.168.1.2"
+
+        ingress.status.load_balancer.ingress = [
+            ip
+        ]
+
         watcher = Mock()
-        
+
         watcher.stream.return_value = [
             {
                 'type': 'ADDED',
                 'object': ingress
             }
         ]
-        
-        networkingApi = Mock()
-        
+
+        networking_api = Mock()
+
         storage_service = Mock(spec=StorageService)
-        
+
         storage_service.find_by_namespace_name_and_ingress_name.return_value = None
-        
+
         zeroconf_service = Mock(spec=ZeroconfService)
-        
-        kubWatcher = KubernetesWatcher(
+
+        kub_watcher = KubernetesWatcher(
             watcher,
-            networkingApi,
+            networking_api,
             storage_service,
             zeroconf_service
         )
-        
-        kubWatcher.start()
-        
+
+        kub_watcher.start()
+
         zeroconf_service.create_record.assert_called_once()
 
-    def test_StartDeletedEvent(self):
+    def test_deleted_ingress(self):
         rule = Mock()
-        
+
         rule.host = "test.local"
-        
+
         ingress = Mock()
-        
+
+        ingress.metadata.namespace = "TestNamespace"
+
+        ingress.metadata.name = "TestIngress"
+
         ingress.spec.rules = [
             rule
         ]
-        
+
+        ip = Mock()
+
+        ip.ip = "192.168.1.2"
+
+        ingress.status.load_balancer.ingress = [
+            ip
+        ]
+
         watcher = Mock()
-        
+
         watcher.stream.return_value = [
             {
                 'type': 'DELETED',
                 'object': ingress
             }
         ]
-        
+
         networkingApi = Mock()
-        
+
         storage_service = Mock(spec=StorageService)
-        
-        storage_service.find_by_namespace_name_and_ingress_name.return_value = Mock(spec=IngressEntity)
-        
+
+        storage_service.find_by_namespace_name_and_ingress_name.return_value = Mock(
+            spec=IngressEntity)
+
         zeroconf_service = Mock(spec=ZeroconfService)
-        
+
         kubWatcher = KubernetesWatcher(
             watcher,
             networkingApi,
             storage_service,
             zeroconf_service
         )
-        
+
         kubWatcher.start()
-        
+
         zeroconf_service.delete_record.assert_called_once()
-        
-    def test_StartCheckModified(self):
-        currentHost = Mock()
-        
-        currentHost.host = "test_new.local"
-        
+
+    def test_modified_ingress(self):
+        rule = Mock()
+
+        rule.host = "test_new.local"
+
         ingress = Mock()
-        
+
+        ingress.metadata.namespace = "TestNamespace"
+
+        ingress.metadata.name = "TestIngress"
+
         ingress.spec.rules = [
-            currentHost
+            rule
         ]
-        
+
+        ip = Mock()
+
+        ip.ip = "192.168.1.2"
+
+        ingress.status.load_balancer.ingress = [
+            ip
+        ]
+
         watcher = Mock()
-        
+
         watcher.stream.return_value = [
             {
                 'type': 'MODIFIED',
                 'object': ingress,
             }
         ]
-        
-        networkingApi = Mock()
-        
+
+        networking_api = Mock()
+
         storage_service = Mock(spec=StorageService)
-        
+
         ingress_entity_mock = Mock(spec=IngressEntity)
-        
+
         ingress_entity_mock.list_mdns_hostnames.return_value = [
             'test_old.local'
         ]
-        
+
         storage_service.find_by_namespace_name_and_ingress_name.return_value = ingress_entity_mock
-        
+
         zeroconf_service = Mock(spec=ZeroconfService)
-        
-        kubWatcher = KubernetesWatcher(
+
+        kub_watcher = KubernetesWatcher(
             watcher,
-            networkingApi,
+            networking_api,
             storage_service,
             zeroconf_service
         )
-        
-        kubWatcher.start()
-        
-        zeroconf_service.remove_hostname_of_record.assert_called_with(ingress_entity_mock, 'test_old.local')
-        
-        zeroconf_service.add_hostname_to_record.assert_called_with(ingress_entity_mock, 'test_new.local')
+
+        kub_watcher.start()
+
+        zeroconf_service.remove_hostname_of_record.assert_called_with(
+            ingress_entity_mock, 'test_old.local')
+
+        zeroconf_service.add_hostname_to_record.assert_called_with(ingress_entity_mock, 'test_new.local', [
+            '192.168.1.2'
+        ])
+
 
 if __name__ == '__main__':
     unittest.main()
