7efa3fbe340ea3a89356436b5e8fb756b477f698
Stephan Höhn <me@steph.ovh>
2023-12-20 23:06:53 +0100

    test: adjusts test to handle argparse

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+from argparse import Namespace
 import unittest
 from unittest.mock import Mock, patch
 from src.kubernetes_utils.kubernetes_watcher import KubernetesWatcher
@@ -8,12 +9,13 @@ from src.zeroconf_utils.zeroconf_service import ZeroconfService
 
 
 class TestMain(unittest.TestCase):
+    @patch("src.core.argparse_factory.ArgparseFactory.create")
     @patch("src.core.core_service.CoreService")
     @patch("src.kubernetes_utils.kubernetes_watcher_factory.KubernetesWatcherFactory.create")
     @patch("src.zeroconf_utils.zeroconf_service_factory.ZeroconfServiceFactory.create")
-    @patch("src.storage.storage_service.StorageService")
+    @patch("src.ingress_storage.ingress_storage.IngressStorage")
     @patch("src.kubernetes_utils.kubernetes_factory.KubernetesFactory.create")
-    def test_main_success(self, kubernetes_factory, storage_service, zc_factory, kw_factory, core_service):
+    def test_main_success(self, kubernetes_factory, storage_service, zc_factory, kw_factory, core_service, argparse_factory):
         kubernetes_service = Mock(spec=KubernetesService)
 
         kubernetes_factory.return_value = kubernetes_service
@@ -25,6 +27,10 @@ class TestMain(unittest.TestCase):
         kubernetes_watcher = Mock(spec=KubernetesWatcher)
 
         kw_factory.return_value = kubernetes_watcher
+        
+        argparse = Mock(config=None)
+        
+        argparse_factory.return_value = argparse
 
         main.main()
         
@@ -32,12 +38,13 @@ class TestMain(unittest.TestCase):
         
         kubernetes_watcher.start.assert_called_once()
     
+    @patch("src.core.argparse_factory.ArgparseFactory.create")
     @patch("src.core.core_service.CoreService")
     @patch("src.kubernetes_utils.kubernetes_watcher_factory.KubernetesWatcherFactory.create")
     @patch("src.zeroconf_utils.zeroconf_service_factory.ZeroconfServiceFactory.create")
-    @patch("src.storage.storage_service.StorageService")
+    @patch("src.ingress_storage.ingress_storage.IngressStorage")
     @patch("src.kubernetes_utils.kubernetes_factory.KubernetesFactory.create") 
-    def test_main_throws_exception(self, kubernetes_factory, storage_service, zc_factory, kw_factory, core_service):
+    def test_main_throws_exception(self, kubernetes_factory, storage_service, zc_factory, kw_factory, core_service, argparse_factory):
         kubernetes_service = Mock(spec=KubernetesService)
 
         kubernetes_factory.return_value = kubernetes_service
@@ -51,6 +58,10 @@ class TestMain(unittest.TestCase):
         kw_factory.return_value = kubernetes_watcher
         
         kubernetes_watcher.start.side_effect = Exception()
+        
+        argparse = Mock(config=None)
+        
+        argparse_factory.return_value = argparse
 
         with self.assertRaises(SystemExit) as cm:
             main.main()
