00646973c996ebd8e0d66605fa377a6f6c439687
Stephan Höhn <me@steph.ovh>
2023-12-20 22:31:48 +0100

    fix: uses better file checking
    the argument reader for --config now uses expanduser so it will recognize the right path even only the file was given

--- src/core/argparse_factory.py
+++ src/core/argparse_factory.py
@@ -9,12 +9,15 @@ class ArgparseFactory:
 
         parser.add_argument("--debug", action="store_true")
 
-        parser.add_argument("--config", action="store", type=validate_file)
+        parser.add_argument("--config", action="store", type=ArgparseFactory.validate_file)
 
         return parser.parse_args()
 
     @staticmethod
-    def validate_file(f):
-        if not os.path.exists(f):
-            raise argparse.ArgumentTypeError("{0} does not exist".format(f))
-        return f
+    def validate_file(file):
+        expanded_path = os.path.expanduser(file)
+        
+        if not os.path.exists(expanded_path):
+            raise argparse.ArgumentTypeError("{0} does not exist".format(file))
+        
+        return expanded_path
